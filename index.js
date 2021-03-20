7c7e9003b222941df3bf7c75991d72e34c69460c
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-03-19 19:43:01 -0500

    Done.

--- index.js
+++ index.js
@@ -17,17 +17,23 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-
+  const appendDriver = [...drivers, name];
+  return appendDriver
 }
 
 function prependDriver(name) {
-
+  const prependDriver = [name, ...drivers]
+  return prependDriver
 }
 
 function removeLastDriver() {
-
+  let removeLastDriver = []
+  let z = removeLastDriver[removeLastDriver.length - 1]
+  removeLastDriver.slice("z")
+  return removeLastDriver
 }
 
 function removeFirstDriver() {
-
+  const removeFirstDriver = []
+  return removeFirstDriver
 }
